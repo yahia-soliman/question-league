3e93fc73ce3ef82cdc77e7ffef2451274cc318b7
Yahia <yn7599@gmail.com>
2024-04-25 09:24:20 +0200

    opentdb.com scraper

---
+++ tests/test_workers/test_opentdb_scraper.py
@@ -0,0 +1,135 @@
+from unittest import mock
+
+from models.question import Question
+from workers import opentdb_scraper as worker
+
+
+@mock.patch("requests.get")
+def test_get_token(mock_get):
+    """Test that the get token function works"""
+    mock_get.return_value.json = lambda: {"token": "testtoken"}
+    token = worker.get_token()
+    assert token == "testtoken"
+
+
+@mock.patch("requests.get")
+def test_get_questions(mock_get):
+    """test that the return is always list"""
+    mock_get.return_value.ok = True
+    mock_get.return_value.json = lambda: {"results": ["a", "b", "c"]}
+    res = worker.get_questions()
+    assert type(res) is list
+    mock_get.return_value.ok = False
+
+    res = worker.get_questions("some_token")
+    assert type(res) is list
+
+
+def test_reformat():
+    """ensure that the reformating is compatible with the question Object"""
+    formatted = worker.reformat(opentdb_result[0])
+    expected = {
+        "points": 200,
+        "category_id": 11,
+        "question": "This trope refers to minor characters that are killed off to show how a monster works.",
+        "right_answer": "Red Shirt",
+        "answers": ["Minions", "Expendables", "Cannon Fodder", "Red Shirt"],
+    }
+    for k, v in formatted.items():
+        assert expected[k] == v
+
+
+def test_store_questions():
+    """test that the function stores the questions into the database"""
+    all_was = len(Question.all())
+    worker.store_questions(opentdb_result)
+    all_is = len(Question.all())
+    assert all_is == all_was + len(opentdb_result)
+
+
+opentdb_result = [
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Film",
+        "question": "This trope refers to minor characters that are killed off to show how a monster works.",
+        "correct_answer": "Red Shirt",
+        "incorrect_answers": ["Minions", "Expendables", "Cannon Fodder"],
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Geography",
+        "question": "There exists an island named &quot;Java&quot;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"],
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "History",
+        "question": "What disease crippled President Franklin D. Roosevelt and led him to help the nation find a cure? ",
+        "correct_answer": "Polio",
+        "incorrect_answers": ["Cancer", "Meningitis", "HIV"],
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Music",
+        "question": "Which one of these Rammstein songs has two official music videos?",
+        "correct_answer": "Du Riechst So Gut",
+        "incorrect_answers": ["Du Hast", "Benzin", "Mein Teil"],
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Comics",
+        "question": "Which Batman sidekick is the son of Talia al Ghul?",
+        "correct_answer": "Damian Wayne",
+        "incorrect_answers": ["Dick Grayson", "Tim Drake", "Jason Todd"],
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Music",
+        "question": "Which of the following bands is Tom DeLonge not a part of?",
+        "correct_answer": "+44",
+        "incorrect_answers": ["Box Car Racer", "Blink-182", "Angels &amp; Airwaves"],
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Video Games",
+        "question": "What engine did the original &quot;Half-Life&quot; run on?",
+        "correct_answer": "GoldSrc",
+        "incorrect_answers": ["Source", "Quake", "Unreal"],
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Comics",
+        "question": "Which universe crossover was introduced in the &quot;Sonic the Hedgehog&quot; comic issue #247?",
+        "correct_answer": "Mega Man",
+        "incorrect_answers": ["Super Mario Brothers", "Alex Kidd", "Super Monkey Ball"],
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Comics",
+        "question": "In Calvin and Hobbes, what is the name of the principal at Calvin&#039;s school?",
+        "correct_answer": "Mr. Spittle",
+        "incorrect_answers": ["Mr. Boreman", "Mr. Spitling", "Mr. Moe"],
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Film",
+        "question": "Which 90&#039;s comedy cult classic features cameos appearances from Meat Loaf, Alice Cooper and Chris Farley?",
+        "correct_answer": "Wayne&#039;s World",
+        "incorrect_answers": [
+            "Bill &amp; Ted&#039;s Excellent Adventure",
+            "Dumb and Dumber",
+            "Austin Powers: International Man of Mystery",
+        ],
+    },
+]
---
+++ workers/opentdb_scraper.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+"""Module to scrape and reformat the question objects from opentdb.com api"""
+
+from datetime import datetime as dt
+from threading import Timer
+
+import requests
+
+from models.question import Question
+
+categories = {
+    "any": 9,
+    "General Knowledge": 9,
+    "Entertainment: Books": 10,
+    "Entertainment: Film": 11,
+    "Entertainment: Music": 12,
+    "Entertainment: Musicals &amp; Theatres": 13,
+    "Entertainment: Television": 14,
+    "Entertainment: Video Games": 15,
+    "Entertainment: Board Games": 16,
+    "Science &amp; Nature": 17,
+    "Science: Computers": 18,
+    "Science: Mathematics": 19,
+    "Mythology": 20,
+    "Sports": 21,
+    "Geography": 22,
+    "History": 23,
+    "Politics": 24,
+    "Art": 25,
+    "Celebrities": 26,
+    "Animals": 27,
+    "Vehicles": 28,
+    "Entertainment: Comics": 29,
+    "Science: Gadgets": 30,
+    "Entertainment: Japanese Anime &amp; Manga": 31,
+    "Entertainment: Cartoon &amp; Animations": 32,
+}
+
+
+def get_token():
+    """get a session token from opentdb"""
+    res = requests.get("https://opentdb.com/api_token.php?command=request")
+    # this token will ensure no duplicates are requested during the session
+    return res.json().get("token")
+
+
+def get_questions(token=None):
+    """get 50 question from the api"""
+    params = {"amount": 50}
+    if token:
+        params["token"] = token
+    res = requests.get("https://opentdb.com/api.php", params=params)
+    if res.ok:
+        return res.json().get("results", [])
+    else:
+        return []
+
+
+def reformat(question: dict):
+    """reformat a question from opentdb to questionleague foramtting"""
+    points = {"hard": 300, "medium": 200, "easy": 100}
+    return {
+        "question": question["question"],
+        "answers": question["incorrect_answers"] + [question["correct_answer"]],
+        "right_answer": question["correct_answer"],
+        "points": points[question.get("difficulty", "easy")],
+        "category_id": categories[question.get("category", "any")],
+    }
+
+
+def store_questions(questions: list):
+    """Store a question league formatted question into the current db storage"""
+    for q in questions:
+        Question(**reformat(q))
+    Question.save()
+
+
+def work(token=None, interval=30):
+    """Do a scraping cycle."""
+    print(dt.now().strftime("%d %h %Y - %H:%M:%S "), end="")
+    print("[WORKER opentdb] fetching...")
+    qs = get_questions(token)
+    if len(qs) == 0:
+        print(dt.now().strftime("%d %h %Y - %H:%M:%S "), end="")
+        print("[WORKER opentdb] exiting... no data found")
+        exit(0)
+    print(dt.now().strftime("%d %h %Y - %H:%M:%S "), end="")
+    print(f"[WORKER opentdb] storing {len(qs)} questions... ", end="")
+    store_questions(qs)
+    print(f"done.")
+    Timer(interval, work, [token, interval]).start()
+
+
+if __name__ == "__main__":
+    import os
+
+    import dotenv
+
+    dotenv.load_dotenv()
+    token = os.getenv("WORKER_TOKEN")
+    if not token:
+        token = get_token()
+        dotenv.set_key(".env", "WORKER_TOKEN", token)
+
+    print(dt.now().strftime("%d %h %Y - %H:%M:%S "), end="")
+    print("[WORKER opentdb] started...")
+    work(token, int(os.getenv("WORKER_INTERVAL", 30)))
