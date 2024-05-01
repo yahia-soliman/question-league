8ca630dea1d9e830b09e7ebcdc1ddb6781a6997e
Yahia <yn7599@gmail.com>
2024-05-02 01:23:26 +0300

    websocket frontend almost ready

--- app/static/scripts/multi.js
+++ app/static/scripts/multi.js
@@ -1,5 +1,4 @@
 class BetterSocket extends WebSocket {
-
   constructor(...props) {
     super(...props);
     this.handlers = {};
@@ -13,7 +12,7 @@ class BetterSocket extends WebSocket {
         event = "message";
         payload = e;
       }
-      this.handlers[event]?.forEach((func) => func(payload))
+      this.handlers[event]?.forEach((func) => func(payload));
     });
   }
 
@@ -21,53 +20,88 @@ class BetterSocket extends WebSocket {
     if (["open", "close", "error", "message"].includes(event)) {
       return this.addEventListener(event, cb);
     }
-    if (!this.handlers[event])
-      this.handlers[event] = [];
+    if (!this.handlers[event]) this.handlers[event] = [];
     this.handlers[event].push(cb);
   }
 
   emit(event, payload) {
-    const message = { event, payload }
-    this.send(JSON.stringify(message))
+    const message = { event, payload };
+    this.send(JSON.stringify(message));
+  }
+
+  unbind(event) {
+    delete this.handlers[event];
+    console.log(this.handlers);
   }
 }
 
+/*
+    <div class="min-h-8 flex group ready items-center justify-between pr-2">
+      <h3 class="font-bold">Ahmed</h3>
+      <span
+        class="hidden border bg-white border-teal-300 rounded-full px-1 text-slate-500 group-[.ready]:inline font-mono text-xs h-min">ready</span>
+    </div>
+*/
 const createUserCard = (user) => {
   let child;
   const card = document.createElement("div");
 
   card.dataset.id = user.user_id;
-  card.classList.add("group");
+  card.className = "min-h-8 flex group items-center justify-between pr-2";
+  if (user.ready) card.classList.append("ready");
 
   child = document.createElement("h3");
   child.innerText = user.user_id;
+  child.className = "font-bold";
   card.append(child);
 
   child = document.createElement("span");
-  child.innerText = "✓";
-  child.classList.add("hidden", "group-[.ready]:inline");
+  child.innerText = "ready";
+  child.className =
+    "hidden border bg-white border-teal-300 rounded-full px-1 text-slate-500 group-[.ready]:inline font-mono text-xs h-min";
   card.append(child);
-
   return card;
-}
+};
 
-const ws = new BetterSocket("ws://" + location.host + location.pathname)
-ws.on("open", () => console.log("connected. via websocket."))
-console.log("connecting...")
-ws.on("message", (e) => console.log(e.data))
+const url = location.host + location.pathname;
+const ws = new BetterSocket("ws://" + url);
+ws.on("open", () => console.log("connected. via websocket."));
+console.log("connecting...");
+ws.on("message", (e) => console.log(e.data));
 window.mysocket = ws;
 
 window.addEventListener("load", () => {
-  const start = document.querySelector("#start-game");
-  const users = document.querySelector("#players");
+  const users = document.getElementById("players");
+  const categories = document.getElementById("categories");
 
-  start.onclick = () => ws.emit("ready");
+  ws.on("welcome", (payload) => {
+    payload.users.map((u) => users.append(createUserCard(u)));
+    const c_ids = Object.keys(payload.categories);
+    c_ids.sort((a, b) => payload.categories[b] - payload.categories[a]);
+    c_ids.map((id) => {
+      const elem = categories.children.find((e) => e.datased.categoryId == id);
+      elem.lastChild.innerText = "- " + "&bullet;".repeat(c.votes);
+      categories.prepend(elem);
+    });
+    ws.unbind("welcome");
+  });
 
   ws.on("user_in", (payload) => {
     users.append(createUserCard(payload));
-  })
+  });
+
+  ws.on("user_out", (payload) => {
+    users.find((el) => el.dataset.user_id == payload)?.remove();
+  });
+
   ws.on("user_ready", (id) => {
     const user = users.querySelector(`[data-id="${id}"]`);
     user.classList.toggle("ready");
-  })
-})
+  });
+
+  document.getElementById("start-game").onclick = () => ws.emit("ready");
+  document.getElementById("invite-link").innerText = url;
+  document.getElementById("copy-link").onclick = () => {
+    navigator.clipboard.writeText(url);
+  };
+});
